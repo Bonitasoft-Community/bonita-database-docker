6fcd3d112dd2b578534f4a2de2616729e2bb234b
Adrien Kantcheff <adrien.kantcheff@bonitasoft.com>
2023-11-07 13:27:30 +0100

    tr(postgres): upgrade postgres from 15.3 to 15.4

---
+++ postgres/15/healthcheck.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# inspired by https://github.com/docker-library/healthcheck
+set -eo pipefail
+
+# check if restore is in progress (see 2_restore_dump.sh)
+if [ -f /var/lib/postgresql/restore.inProgress ]; then
+  echo "Database restore in progress. Not yet healthy."
+  exit 1
+fi
+
+user="${POSTGRES_USER:-bonita}"
+db="${POSTGRES_DB:-$user}"
+export PGPASSWORD="${POSTGRES_PASSWORD:-bpm}"
+
+args=(
+  # force postgres to not use the local unix socket (test "external" connectivity)
+  --host "$host"
+  --username "$user"
+  --dbname "$db"
+  --quiet --no-align --tuples-only
+)
+
+if select="$(echo 'SELECT 1' | psql "${args[@]}")" && [ "$select" = '1' ]; then
+  # host is healthy
+  exit 0
+fi
+
+# host is not healthy
+exit 1
