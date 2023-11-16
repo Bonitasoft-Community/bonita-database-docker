395cbe356433ad0ef23aea6d537138fda713bac4
Adrien Kantcheff <5028967+akantcheff@users.noreply.github.com>
2023-11-16 10:04:28 +0100

    tr(postgres): upgrade postgres from 15.3 to 15.4 (#12)
    - Upgrade postgres base image from `postgres:15.3-alpine` to `postgres:15.4-alpine`
    - Fix healthcheck (do not use `pg_isready` as it returns false positive)

--- postgres/15/1_update-postgresql-conf.sh
+++ postgres/15/1_update-postgresql-conf.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-#max_prepared_transactions
+# set max_prepared_transactions to 100
 sed -i "s/^.*max_prepared_transactions\s*=\s*\(.*\)$/max_prepared_transactions = 100/" "$PGDATA"/postgresql.conf
